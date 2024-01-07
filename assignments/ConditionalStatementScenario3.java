4268db729ffefa1cfd836ce9edc75c669b08ad06
TharunkumarPT <155864490+TharunkumarPT@users.noreply.github.com>
2024-01-07 09:02:11 +0530

    Add files via upload
    Class1 Day1 Assignments

---
+++ assignments/ConditionalStatementScenario3.java
@@ -0,0 +1,24 @@
+package class1.day1.assignments;
+
+public class ConditionalStatementScenario3 {
+
+	public static void main(String[] args) {
+		
+		int a=-5;
+		
+		if(a>0)
+		{
+			System.out.println("The value is positive");
+		}
+		else if(a==0)
+		{
+			System.out.println("The value is Neutral");
+		}
+		else
+		{
+			System.out.println("The value is Negative");
+		}
+
+	}
+
+}
